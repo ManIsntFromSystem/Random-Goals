00fc8612ed6b19fd3d12ac7b2b288a0213981af7
ManIsntFromSystem <dimuch.logan@mail.com>
2020-06-15 09:44:03 +0300

    All is working perfectly

--- app/src/main/java/com/quantumman/randomgoals/model/Goal.kt
+++ app/src/main/java/com/quantumman/randomgoals/model/Goal.kt
@@ -1,6 +1,6 @@
 package com.quantumman.randomgoals.model
 
-data class Goal(val id: Int = 0,
+data class Goal(val id: Int = -1,
                 val nameListGoals: String = "",
                 val nameGoal: String = "",
                 val iconGoal: String = "") {}
---
+++ app/src/main/java/com/quantumman/randomgoals/model/ListNames.kt
@@ -0,0 +1,7 @@
+package com.quantumman.randomgoals.model
+
+data class ListNames(
+    private val id: Int = -1,
+    private val nameList: String = "",
+    private val listIcon: String = ""
+) {}
